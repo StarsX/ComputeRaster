ee68b439432537b58eb5ed4b44031f996ac34f5a
StarsX <X-Universe@outlook.com>
2021-02-06 13:48:22 +0800

    XUSG updated for uploader fix.

--- ComputeRaster/ComputeRaster.cpp
+++ ComputeRaster/ComputeRaster.cpp
@@ -217,10 +217,10 @@ void ComputeRaster::OnKeyUp(uint8_t key)
 {
 	switch (key)
 	{
-	case 0x20:	// case VK_SPACE:
+	case VK_SPACE:
 		m_pausing = !m_pausing;
 		break;
-	case 0x70:	//case VK_F1:
+	case VK_F1:
 		m_showFPS = !m_showFPS;
 		break;
 	}
