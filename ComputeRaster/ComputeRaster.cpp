38dcb343664d40f6b3cf3470a2b4f920c27f7788
StarsX <X-Universe@outlook.com>
2023-05-03 16:00:37 +0800

    XUSG fix of root signature highest version for vkd3d-proton

--- ComputeRaster/ComputeRaster.cpp
+++ ComputeRaster/ComputeRaster.cpp
@@ -23,7 +23,8 @@ ComputeRaster::ComputeRaster(uint32_t width, uint32_t height, std::wstring name)
 	m_pausing(false),
 	m_tracking(false),
 	m_meshFileName("Assets/bunny.obj"),
-	m_meshPosScale(0.0f, 0.0f, 0.0f, 1.0f)
+	m_meshPosScale(0.0f, 0.0f, 0.0f, 1.0f),
+	m_screenShot(0)
 {
 #if defined (_DEBUG)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
--- ComputeRaster/ComputeRaster.h
+++ ComputeRaster/ComputeRaster.h
@@ -59,10 +59,6 @@ private:
 	XMFLOAT3	m_focusPt;
 	XMFLOAT3	m_eyePt;
 
-	// Screen-shot helper
-	XUSG::Buffer::uptr	m_readBuffer;
-	uint32_t			m_rowPitch;
-
 	// Synchronization objects.
 	uint32_t	m_frameIndex;
 	HANDLE		m_fenceEvent;
@@ -82,8 +78,10 @@ private:
 	std::string m_meshFileName;
 	XMFLOAT4 m_meshPosScale;
 
-	// Screen-shot state
-	uint8_t m_screenShot;
+	// Screen-shot helpers and state
+	XUSG::Buffer::uptr	m_readBuffer;
+	uint32_t			m_rowPitch;
+	uint8_t				m_screenShot;
 
 	void LoadPipeline();
 	void LoadAssets();
